86e4a8d6336d2dbe6a11f1e346522d8c3888b6ca
aaryannagpal <aaryannagpal65@gmail.com>
2023-10-25 22:38:01 +0530

    Print removed

--- Assignments/26_A3/26_A3.c
+++ Assignments/26_A3/26_A3.c
@@ -5,12 +5,5 @@ extern void yyerror(char *s);
 
 int main(){
     int parser = yyparse();
-
-    if (parser == 0){
-        printf("Parsing successful\n");
-    } else {
-        printf("Parsing failed\n");
-    }
-
     return 0;
 }
